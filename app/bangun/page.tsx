6e8fb0e42d38163447ad836c268a1564da0c9cd8
hayasehiise <kuronegahiise@gmail.com>
2024-03-17 20:03:48 +0800

    Fixed Desain Page and add Bangun Page

---
+++ app/bangun/page.tsx
@@ -0,0 +1,15 @@
+import { SectionMain } from "./components/sectionBangun"
+
+
+
+const Bangun = () => {
+    return(
+        <>
+        <main className="container-xl">
+            <SectionMain />
+        </main>
+        </>
+    )
+}
+
+export default Bangun
