a932b0dc4d415015700c73d205ad0221eedd0f8f
hayasehiise <kuronegahiise@gmail.com>
2024-03-21 11:14:32 +0800

    Little Update

--- app/interior/components/sectionInterior.tsx
+++ app/interior/components/sectionInterior.tsx
@@ -9,6 +9,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
+import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -107,7 +108,7 @@ export default function SectionCarousel() {
               </motion.p>
             </div>
             <div className="xl:mt-5 xl:w-full justify-center items-center">
-              <Carousel className="xl:w-[70%] mx-auto">
+              <Carousel className="xl:w-[70%] mx-auto" plugins={[Autoplay({ delay: 3000 })]}>
                 <CarouselContent>
                   {carouselProjectItem.map((item, index) => (
                     <CarouselItem className="basis-1/2" key={index}>
@@ -157,37 +158,3 @@ export default function SectionCarousel() {
     </Section>
   );
 }
-// function Section({ children }: SectionProps) {
-//   return <div className="flex flex-col">{children}</div>;
-// }
-
-// export function SectionMain() {
-//   return (
-//     <Section>
-//   <div className="w-full h-screen bg-[url(/image/interior/background.jpg)] bg-cover bg-center">
-//     <div className="flex xl:flex-row-reverse h-screen w-full">
-//       <div className="xl:w-full xl:my-auto xl:mx-36">
-//         <p className="text-black xl:text-7xl font-black text-right">
-//           Serahkan Keindahan Ruang Anda: Selamat Datang di Dunia Desain
-//           Interior yang Menginspirasi!
-//         </p>
-//       </div>
-//       <div className="xl:w-full xl:my-auto xl:mx-96">
-//         <Button variant="transparent" size="xl" asChild>
-//           <Link href="#" className="text-xl font-bold">
-//             Let's Check out{" "}
-//             <Image
-//               src="./icons/arrow-down.svg"
-//               alt="arrow down"
-//               width={35}
-//               height={35}
-//               className="pl-2"
-//             />
-//           </Link>
-//         </Button>
-//       </div>
-//     </div>
-//   </div>
-//     </Section>
-//   );
-// }
