57beeb5bc33568066fa722e5bae44f245dd17276
hayasehiise <kuronegahiise@gmail.com>
2024-03-21 10:46:58 +0800

    LP Interior done(?)

--- app/interior/components/sectionInterior.tsx
+++ app/interior/components/sectionInterior.tsx
@@ -5,6 +5,8 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/carousel";
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
 import Image from "next/image";
@@ -15,6 +17,18 @@ import React from "react";
 interface SectionProps {
   children: React.ReactNode;
 }
+
+type carouselProjectType = {
+  image: string;
+  alt: string;
+};
+
+const carouselProjectItem: carouselProjectType[] = [
+  { image: "/image/interior/carousel/item1.jpg", alt: "item1" },
+  { image: "/image/interior/carousel/item2.jpg", alt: "item2" },
+  { image: "/image/interior/carousel/item3.jpg", alt: "item3" },
+  { image: "/image/interior/carousel/item4.jpg", alt: "item4" },
+];
 
 function Section({ children }: SectionProps) {
   return (
@@ -72,7 +86,8 @@ export default function SectionCarousel() {
                 Ubah Ruangan Dalam Rumah Anda
               </motion.p>
               <p className="p-5 xl:text-lg text-center">
-                Kami memiliki pengalaman yang dalam dalam mendesain & membuat interior untuk segala kebutuhan
+                Kami memiliki pengalaman yang dalam dalam mendesain & membuat
+                interior untuk segala kebutuhan
               </p>
             </div>
           </div>
@@ -81,7 +96,7 @@ export default function SectionCarousel() {
       <CarouselItem>
         <div className="w-full h-screen bg-[url(/image/interior/background3.jpg)] bg-cover bg-center">
           <div className="flex flex-col xl:h-screen xl:w-full">
-            <div className="xl:mt-16">
+            <div className="xl:mt-16 xl:w-full">
               <motion.p
                 initial={{ y: "-100%", opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
@@ -91,8 +106,50 @@ export default function SectionCarousel() {
                 Project Interior
               </motion.p>
             </div>
-            <div className="xl:mt-5 justify-center items-center text-center">
-                Isi Project Interior
+            <div className="xl:mt-5 xl:w-full justify-center items-center">
+              <Carousel className="xl:w-[70%] mx-auto">
+                <CarouselContent>
+                  {carouselProjectItem.map((item, index) => (
+                    <CarouselItem className="basis-1/2" key={index}>
+                      <Image
+                        src={item.image}
+                        alt={item.alt}
+                        width={1000}
+                        height={1000}
+                        className="object-cover rounded-full"
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselNext variant='secondary' className="h-24" />
+                <CarouselPrevious variant='secondary' className="h-24" />
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </CarouselItem>
+      <CarouselItem>
+        <div className="w-full h-screen bg-[url(/image/interior/background4.jpg)] bg-cover bg-center">
+          <div className="flex flex-col xl:h-screen xl:w-full">
+            <div className="xl:mt-80 xl:mx-auto rounded-xl bg-white w-fit p-5 justify-center items-center text-center bg-opacity-70">
+              <p
+                className="text-black xl:text-5xl font-black"
+              >
+                Yuk, Mulai Rencanakan Interiormu Sekarang
+              </p>
+              <p className="pt-3 xl:text-lg">
+                Dengan konsultasi melalui WhatsApp tim kami akan memberikan hasil yang terbaik untuk perencanaan interior anda
+              </p>
+              <Button variant="success" className="rounded-full p-6 mt-5" asChild>
+              <Link
+                href="https://api.whatsapp.com/send?phone=6281352294435&text=Saya%20ingin%20Rancang%20Interior%20"
+                target="_blank"
+                className=" text-xl"
+              >
+                Mulai Konsultasi
+                <img src="./image/wa_icon.png" className="h-8 ml-2" />
+              </Link>
+            </Button>
             </div>
           </div>
         </div>
