c78de4e72b2f5e72b5fa04c8c002a8cbb011d133
hayasehiise <kuronegahiise@gmail.com>
2024-03-27 10:18:58 +0800

    Fix Homepage

--- components/homeSection.tsx
+++ components/homeSection.tsx
@@ -12,7 +12,7 @@ interface SectionProps {
 
 const fullScreenCarousel: CustomFlowbiteTheme["carousel"] = {
   scrollContainer: {
-    base: "flex w-full xl:h-[700px] sm:h-96 xs:h-56 snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth",
+    base: "flex w-full xl:h-[750px] sm:h-96 xs:h-56 snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth",
   },
 };
 
@@ -50,38 +50,23 @@ const carouselImageData: carouselImageType[] = [
 const myServiceData: myServiceType[] = [
   {
     title: "Ingin Jadi Agen?",
-    images: "./image/homepage/agen_icon.png",
+    images: "./image/homepage/agen.png",
     links: "/agen",
   },
   {
     title: "Ingin Desain Rumah?",
-    images: "./image/homepage/desain_icon.png",
+    images: "./image/homepage/desain.png",
     links: "/desain",
   },
   {
     title: "Ingin Bangun Rumah?",
-    images: "./image/homepage/bangun_rumah_icon.png",
+    images: "./image/homepage/bangun.png",
     links: "/bangun",
   },
   {
     title: "Ingin Desain & Bangun Interior?",
-    images: "./image/homepage/interior_icon.png",
+    images: "./image/homepage/interior.png",
     links: "/interior",
-  },
-  {
-    title: "Ingin Jual Rumah?",
-    images: "./image/homepage/jual_rumah_icon.png",
-    links: "#",
-  },
-  {
-    title: "Ingin Bahan Bangunan?",
-    images: "./image/homepage/material_icon.png",
-    links: "#",
-  },
-  {
-    title: "Ingin info kerja?",
-    images: "./image/homepage/kerja_icon.png",
-    links: "#",
   },
   {
     title: "Ingin tau tentang infinity?",
@@ -106,7 +91,7 @@ export function SectionMain() {
               height="100%"
             />
           </div>
-          <div className="absolute w-full h-screen xl:bg-gradient-to-r sm:bg-gradient-to-t xs:bg-gradient-to-t from-black xl:from-55% sm:from-30% xs:from-55% z-20 flex flex-col xl:justify-center xl:items-start sm:justify-end sm:items-center xs:justify-end xs:items-center">
+          <div className="relative w-full h-screen xl:bg-gradient-to-r sm:bg-gradient-to-t xs:bg-gradient-to-t from-black xl:from-55% sm:from-30% xs:from-55% z-20 flex flex-col xl:justify-center xl:items-start sm:justify-end sm:items-center xs:justify-end xs:items-center">
             <p className="text-white xl:text-7xl sm:text-7xl xs:text-4xl font-extrabold xl:mx-24 sm:-ml-28 xs:-ml-16">
               AWALUDIN
             </p>
@@ -135,7 +120,7 @@ export function SectionCarousel() {
     <Section>
       <Carousel indicators={false} theme={fullScreenCarousel}>
         {carouselImageData.map((item, index) => (
-          <img src={item.images} key={index} className="h-full object-cover" />
+          <img src={item.images} key={index} className="h-full object-cover object-center" />
         ))}
       </Carousel>
     </Section>
@@ -145,37 +130,80 @@ export function SectionCarousel() {
 export function SectionService() {
   return (
     <Section>
-      <div className="w-full max-h-screen">
-        <div className="relative">
-          <img
-            src="/image/homepage/service.jpg"
-            className="absolute w-fit xl:h-[700px] object-contain scale-x-[-1] xl:left-0 z-10"
-          />
-          <div className="absolute w-full xl:h-[700px] sm:h-[900px] xs:h-[500px] xl:bg-gradient-to-l sm:bg-gradient-to-t xs:bg-gradient-to-t from-gray-100 xl:from-50% sm:from-50% xs:from-60% z-20 flex flex-col xl:justify-center xl:items-end sm:justify-end sm:items-center xs:justify-end xs:items-center">
-            <div className="xl:w-1/2 sm:w-1/2 xs:w-full flex flex-col justify-center items-center p-5">
-              <p className="xl:text-5xl sm:text-5xl xs:text-2xl font-extrabold">MY SERVICE</p>
-              <Carousel
-                pauseOnHover
-                indicators={false}
-                theme={myServiceCarousel}
-                className="xl:h-96 sm:h-96 xs:h-64 p-5"
-              >
-                {myServiceData.map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex flex-col items-center justify-center text-black"
-                  >
-                    <img src={item.images} className="xl:w-60 sm:w-52 xs:w-32" />
-                    <p className="py-3">{item.title}</p>
-                    <Link href={item.links}>
-                      <Button pill color="amber" theme={myServiceButton}>
-                        Klik Disini
-                      </Button>
-                    </Link>
-                  </div>
-                ))}
-              </Carousel>
-            </div>
+      <div className="w-full h-full">
+        <img
+          src="/image/homepage/service.jpg"
+          className="absolute w-fit xl:h-[700px] object-contain scale-x-[-1] xl:left-0 z-10"
+        />
+        <div className="relative w-full xl:h-[700px] sm:h-[900px] xs:h-[500px] xl:bg-gradient-to-l sm:bg-gradient-to-t xs:bg-gradient-to-t from-gray-100 xl:from-50% sm:from-50% xs:from-60% z-20 flex flex-col xl:justify-center xl:items-end sm:justify-end sm:items-center xs:justify-end xs:items-center">
+          <div className="xl:w-1/2 sm:w-1/2 xs:w-full flex flex-col justify-center items-center p-5">
+            <p className="xl:text-5xl sm:text-5xl xs:text-2xl font-extrabold">
+              MY SERVICE
+            </p>
+            <Carousel
+              pauseOnHover
+              indicators={false}
+              theme={myServiceCarousel}
+              className="xl:w-[700px] sm:w-[500px] xs:w-[350px] xl:h-[500px] sm:h-96 xs:h-64 p-5"
+            >
+              {myServiceData.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col items-center justify-center text-black"
+                >
+                  <img src={item.images} className="xl:h-80 sm:h-56 xs:h-32" />
+                  <p className="py-3">{item.title}</p>
+                  <Link href={item.links}>
+                    <Button pill color="amber" theme={myServiceButton}>
+                      Klik Disini
+                    </Button>
+                  </Link>
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+export function SectionFooter() {
+  return (
+    <Section>
+      <div className="w-full h-full bg-amber-400">
+        <div className="flex flex-row">
+          <div className="flex flex-col m-5">
+            <p className="text-3xl font-bold border-b-4 border-black">
+              My Social Media
+            </p>
+            <Link href={"https://www.instagram.com/awaludin_infinity/"} target="_blank">
+            <p className="flex items-center mt-3">
+              <img src="/image/homepage/instagram.png" className="h-10 pr-2" />
+              awaludin_infinity
+            </p>
+            </Link>
+            <Link href={"https://www.youtube.com/@awaludin1832"} target="_blank">
+            <p className="flex items-center mt-3">
+              <img src="/image/homepage/youtube.png" className="h-10 pr-2" />
+              @awaludin1832
+            </p>
+            </Link>
+            <Link href={"https://www.tiktok.com/@awaludin_infinity?lang=id-ID"} target="_blank">
+            <p className="flex items-center mt-3">
+              <img src="/image/homepage/tiktok.png" className="h-10 pr-2" />
+              @awaludin_infinity
+            </p>
+            </Link>
+          </div>
+          <div className="flex flex-col m-5">
+            <p className="text-3xl font-bold border-b-4 border-black">
+              My Contact
+            </p>
+            <p className="flex items-center mt-3">
+              <img src="/image/homepage/whatsapp.png" className="h-10 pr-2" />
+              0822-2319-9958
+            </p>
           </div>
         </div>
       </div>
