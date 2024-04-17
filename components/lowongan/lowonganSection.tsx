0e49f68e852b2299a90a37018eaadfb3d4042fff
hayasehiise <kuronegahiise@gmail.com>
2024-04-17 11:33:06 +0800

    FIX INI MAH

--- components/lowongan/lowonganSection.tsx
+++ components/lowongan/lowonganSection.tsx
@@ -70,7 +70,7 @@ export function SectionJob() {
   //   "http://127.0.0.1:8000/api/lowongan"
   // );
   const [currentApi, setCurrentApi] = useState(
-    `${process.env.API_URL}`
+    `${process.env.API_URL ? process.env.API_URL : process.env.NEXT_PUBLIC_API_URL}`
   );
 
   console.log(currentApi)
@@ -78,7 +78,7 @@ export function SectionJob() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(currentApi);
+        const res = await axios.get(currentApi, {headers : {'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT'}});
         // console.log(res.data.data);
         setData(res.data.data.data);
         setDataLink(
