c22f9379900425e3630c880d3c37cd5e09ae7e75
hayasehiise <kuronegahiise@gmail.com>
2024-04-29 15:15:16 +0800

    fix dikit lagi

--- components/interior/interiorSection.tsx
+++ components/interior/interiorSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import { Button } from "@components/ui/button";
 import Link from "next/link";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef, useState, useEffect } from "react";
