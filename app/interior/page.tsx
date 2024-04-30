1deb654e538699152079ee8258e290220508d88f
hayasehiise <kuronegahiise@gmail.com>
2024-04-30 08:54:26 +0800

    change background interior

--- app/interior/page.tsx
+++ app/interior/page.tsx
@@ -11,7 +11,8 @@ import {
 export default function Interior() {
   return (
     <>
-    <div className="bg-gradient-to-b from-stone-600 via-stone-400 to-stone-100 bg-fixed">
+    {/* <div className="bg-gradient-to-b from-stone-600 via-stone-400 to-stone-100 bg-fixed"> */}
+    <div className="sm:bg-[url(/image/interior/background1.jpg)] bg-[url(/image/interior/background1-phone.jpg)] bg-cover bg-fixed">
     <SectionMain />
     <SectionSecond />
     </div>
