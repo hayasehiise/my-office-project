0fa7ceb8be1e77f5938317b7e893b2a7f7a125f8
hayasehiise <kuronegahiise@gmail.com>
2024-04-30 09:47:46 +0800

    lagi update accordion

--- components/website/faqAccordion.tsx
+++ components/website/faqAccordion.tsx
@@ -14,8 +14,8 @@ export default function FaqAccordion({question, children, value}: {question: str
 
     return(
           <AccordionItem value={value} className="mb-3">
-            <AccordionTrigger aria-pressed={isPressed} onClick={() => handleClick()} className="bg-slate-950 text-white rounded-xl aria-pressed:rounded-bl-none aria-pressed:rounded-br-none px-5">{question}</AccordionTrigger>
-            <AccordionContent className="bg-white p-5 rounded-bl-xl rounded-br-xl">
+            <AccordionTrigger aria-pressed={isPressed} onClick={() => handleClick()} className="bg-slate-950 text-white text-left rounded-xl aria-pressed:rounded-bl-none aria-pressed:rounded-br-none px-5">{question}</AccordionTrigger>
+            <AccordionContent className="bg-white p-5 rounded-bl-xl rounded-br-xl text-left">
               {children}
             </AccordionContent>
           </AccordionItem>
